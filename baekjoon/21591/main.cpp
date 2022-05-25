62a2a179bd7149a2a0fd8676ce31536cb30511ac
Bammer <jhcse5189@gmail.com>
2022-05-25 14:47:34 +0000

    Add. BJ/21591

---
+++ baekjoon/21591/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int wc, hc, ws, hs;
+	cin >> wc >> hc >> ws >> hs;
+	
+	if ((wc - ws < 2) || (hc - hs < 2))
+		cout << 0 << endl;
+	else
+		cout << 1 << endl;
+	
+	return 0;
+}
