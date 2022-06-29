091bcf7a879542a75106cb1685c4795fafe3d7b2
Bammer <jhcse5189@gmail.com>
2022-06-29 22:37:16 +0900

    Add. BJ/2566

--- baekjoon/2566/main.cpp
+++ baekjoon/2566/main.cpp
@@ -2,29 +2,22 @@
 #include <algorithm>
 using namespace std;
 
+int a[10][10];
+
 int main()
 {
-	int** a = new int*[10];
-	for (int i = 0; i < 10; i++)
-		a[i] = new int[10]();
-	
-	for (int i = 1; i < 10; i++)
-	{
-		for (int j = 1; j < 10; j++)
-		{
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
 			cin >> a[i][j];
 		}
 	}
 	
 	int max = 0;
-	int idx = 0, jdx = 0;
+	int idx = 1, jdx = 1;
 	
-	for (int i = 1; i < 10; i++)
-	{
-		for (int j = 1; j < 10; j++)
-		{
-			if (max < a[i][j])
-			{
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			if (max < a[i][j]) {
 				max = a[i][j];
 				idx = i;
 				jdx = j;
@@ -34,8 +27,6 @@ int main()
 		
 	cout << max << endl
 		 << idx << ' ' << jdx << endl;
-	
-	delete a;
-	
+
 	return 0;
 }
