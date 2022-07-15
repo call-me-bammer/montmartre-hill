67bb7017e3bf82c9898ef3fb08dbb88d42ab15af
Bammer <jhcse5189@gmail.com>
2022-07-15 13:22:31 +0000

    Add. BJ/17614

---
+++ baekjoon/17614/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	
+	int count = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		string s = to_string(i);
+		
+		for (int j = 0; j < s.size(); j++) {
+			if ((s[j] == '3') || (s[j] == '6') || (s[j] == '9'))
+				count++;
+		}
+	}
+	
+	cout << count << endl;
+	return 0;
+}
