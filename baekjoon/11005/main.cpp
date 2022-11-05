e14f5364576b5c898306e966dd992edb2414de77
Bammer <jhcse5189@gmail.com>
2022-11-05 20:58:29 +0900

    Add. BJ/11005

---
+++ baekjoon/11005/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+ll powerOfInt(int b, int p);
+
+int main() {
+  ll n;
+  int b;
+  cin >> n >> b;
+
+  ll p = 0;
+  while (n >= powerOfInt(b, p))
+    p++;
+  p--;
+
+  if (p < 0) {
+    cout << n;
+  }
+
+  while (p >= 0) {
+    ll c = n / powerOfInt(b, p);
+    n = n % powerOfInt(b, p);
+    p--;
+
+    if (c >= 10)
+      cout << (char)('A' + c - 10);
+    else
+      cout << c;
+  }
+  cout << '\n';
+
+  return 0;
+}
+
+ll powerOfInt(int b, int p) {
+  ll out = 1;
+  for (int i = 0; i < p; i++) {
+    out *= b;
+  }
+  return out;
+}
