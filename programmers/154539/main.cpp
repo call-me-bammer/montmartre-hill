856b6834b55d4d2869c7cc6571cf6bcc38b8e64a
bammer <jhcse5189@gmail.com>
2023-03-13 16:46:39 +0900

    Comment: main.cpp of prog/154539: 뒤에 있는 큰 수 찾기

--- programmers/154539/main.cpp
+++ programmers/154539/main.cpp
@@ -10,15 +10,23 @@ typedef pair<int, int> node;
 vector<int> solution(vector<int> numbers) {
     vector<int> answer(numbers.size());
 
+    // 뒷 큰수를 찾아야 하는 칭구들, min heap
     priority_queue<node, vector<node>, greater<node>> pq;
     pq.push(make_pair(numbers[0], 0));
 
+    // 뒷 큰수가 될 후보들
     for (int i = 1; i < numbers.size(); i++) {
+
+        cout << "first top : " << pq.top().first << '\n';
 
         while ((!pq.empty()) && (pq.top().first < numbers[i])) {
             answer[pq.top().second] = numbers[i];
             pq.pop();
         }
+
+        for (int j = 0; j < numbers.size(); j++)
+            cout << answer[j] << ' ';
+        cout << '\n';
 
         pq.push(make_pair(numbers[i], i));
     }
