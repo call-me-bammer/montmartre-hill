ccc41647d0769af8bf50f8d50952e027e086d47b
call-me-bammer <jhcse5189@gmail.com>
2025-01-24 18:47:08 +0900

    add: BJ/15654: N과 M (5)

---
+++ baekjoon/15654/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void backtrack(vector<int> v, vector<int> &r);
+
+int n, m;
+
+int main() {
+  cin >> n >> m;
+
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end());
+
+  vector<int> r;
+  backtrack(v, r);
+
+  return 0;
+}
+
+void backtrack(vector<int> v, vector<int> &r) {
+  if (r.size() == m) {
+    for (int i = 0; i < m; i++) {
+      cout << r[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (find(r.begin(), r.end(), v[i]) != r.end()) {
+      continue;
+    }
+    r.push_back(v[i]);
+    backtrack(v, r);
+    r.pop_back();
+  }
+}
