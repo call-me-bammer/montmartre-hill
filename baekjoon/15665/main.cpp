5fc6bdf1f15f00cea0ec13129544457681a2e0c8
call-me-bammer <jhcse5189@gmail.com>
2025-01-30 06:23:45 +0900

    add: BJ/15665: N과 M (11)

---
+++ baekjoon/15665/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+void backtrack(vector<int> v, vector<int> &r);
+
+int n, m;
+set<vector<int>> s;
+
+int main() {
+  cin >> n >> m;
+
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end());
+
+  vector<int> r;
+  backtrack(v, r);
+
+  for (auto i : s) {
+    for (auto j : i) {
+      cout << j << ' ';
+    }
+    cout << '\n';
+  }
+
+  return 0;
+}
+
+void backtrack(vector<int> v, vector<int> &r) {
+  if (r.size() == m) {
+    s.insert(r);
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    r.push_back(v[i]);
+    backtrack(v, r);
+    r.pop_back();
+  }
+}
