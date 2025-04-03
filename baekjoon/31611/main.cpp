e1aa2a54b711ed163709423c9b5fec69de7d8eea
call-me-bammer <jhcse5189@gmail.com>
2025-04-03 19:45:31 +0900

    add: BJ/31611: 火曜日 (Tuesday)

---
+++ baekjoon/31611/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+
+  if (x % 7 == 2) {
+    cout << 1 << endl;
+  } else {
+    cout << 0 << endl;
+  }
+
+  return 0;
+}
