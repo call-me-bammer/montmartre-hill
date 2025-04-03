59aa43eb9141d57f049705ac1978c189fa2106eb
call-me-bammer <jhcse5189@gmail.com>
2025-04-03 22:45:55 +0900

    add: BJ/11121: Communication Channels

---
+++ baekjoon/11121/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    string a, b;
+    cin >> a >> b;
+    if (a == b) {
+      cout << "OK\n";
+    } else {
+      cout << "ERROR\n";
+    }
+  }
+  return 0;
+}
