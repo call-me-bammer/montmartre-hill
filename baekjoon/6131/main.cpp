a50d17a62cf33f8478d653c7c21e9522baeaf643
call-me-bammer <jhcse5189@gmail.com>
2025-06-20 13:53:00 +0900

    add: BJ/6131: 완전 제곱수

---
+++ baekjoon/6131/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int count = 0;
+  for (int a = 1; a <= 500; a++) {
+    for (int b = 1; b <= a; b++) {
+      if (a * a == b * b + n) {
+        count++;
+      }
+    }
+  }
+  cout << count << endl;
+  return 0;
+}
