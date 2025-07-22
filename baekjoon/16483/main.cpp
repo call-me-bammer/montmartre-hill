cd6de999d1d10b0009aa2e4b26c7954313a4e415
call-me-bammer <jhcse5189@gmail.com>
2025-07-23 06:38:31 +0900

    add: BJ/16483: 접시 안의 원

---
+++ baekjoon/16483/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double t;
+  cin >> t;
+  cout << (int)round(pow(t, 2) / 4.0) << endl;
+  return 0;
+}
