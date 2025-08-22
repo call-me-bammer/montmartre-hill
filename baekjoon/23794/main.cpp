fc47d74eaec1cd5b8594e018c081028e19e017b3
call-me-bammer <jhcse5189@gmail.com>
2025-08-22 17:17:55 +0900

    add: BJ/23794: 골뱅이 찍기 - 정사각형

---
+++ baekjoon/23794/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n + 2; i++) {
+    for (int j = 1; j <= n + 2; j++) {
+      if (j == 1 || j == n + 2) {
+        cout << '@';
+        continue;
+      }
+
+      if (i == 1 || i == n + 2) {
+        cout << '@';
+        continue;
+      }
+
+      cout << ' ';
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
