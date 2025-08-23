6553cc1f1e65c99f351468bfb900891f218d0c29
call-me-bammer <jhcse5189@gmail.com>
2025-08-23 16:51:43 +0900

    add: BJ/23802: 골뱅이 찍기 - 뒤집힌 ㄱ

---
+++ baekjoon/23802/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n * 5; i++) {
+    if (i < n) {
+      for (int j = 0; j < n * 5; j++) {
+        cout << '@';
+      }
+      cout << endl;
+    } else {
+      for (int j = 0; j < n; j++) {
+        cout << '@';
+      }
+      cout << endl;
+    }
+  }
+  return 0;
+}
